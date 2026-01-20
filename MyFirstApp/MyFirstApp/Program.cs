edd9f31e8e07dd180430c6e943d8cf1e038b87c1
hkamaliya <Hardik.Kamaliya@stridelysolutions.com>
2026-01-21 00:00:29 +0530

    Update greeting message in Main method
    Changed the console output in Program.cs from "Hello World !" to "Hello Hardik" to personalize the greeting.

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -10,7 +10,7 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
+            Console.WriteLine("Hello Hardik");
         }
     }
 }
