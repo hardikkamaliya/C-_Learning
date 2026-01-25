6c7c41d890763e654cceef031c00b08a12557709
hkamaliya <Hardik.Kamaliya@stridelysolutions.com>
2026-01-25 20:08:27 +0530

    Sec_-1 added

---
+++ S-1_C#_Language_Basics/S-1_C#_Language_Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_1_C__Language_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
