ac1603fd0f300d6de1feb248fff9ed884eb01df5
hkamaliya <Hardik.Kamaliya@stridelysolutions.com>
2026-02-09 16:39:13 +0530

    learn operators and data types

--- S-1_C#_Language_Basics/S-1_C#_Language_Basics/Program.cs
+++ S-1_C#_Language_Basics/S-1_C#_Language_Basics/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace S_1_C__Language_Basics
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Printing statement in one line 
+
+            //Console.WriteLine("Hello, Guys!");
+            //Console.WriteLine("How are you");
+            Console.WriteLine("let's have fun tonight 😁");
+
+            //// Printing statement in one line 
+            //Console.Write("Hello,Hardik");
+            //Console.Write("Have you prepared for tommorow debeat ?");
+
+            ////waiting for pressing key in keyboard
+            //Console.ReadKey();
+
+            ////Clear Console Screen 
+
+            //Console.Clear();
+
+            //Console.WriteLine("by by!");
+            //Console.ReadKey();
+
+            //===== Variable ================
+
+            //======= Data Types  ==========
+
+
+            // Unidcode is superset of ASCII, Because ASCII Support only English Language Character
+            // While Unicode Support all lnaguage characters.
+
+            //sbyte a = sbyte.MaxValue;
+            //sbyte b = sbyte.MinValue;
+            //sbyte c = default(sbyte);
+            //ushort s = default(ushort);
+            //ushort m = ushort.MaxValue;
+            //ushort n = ushort.MinValue;
+
+            //other data types  :
+            // ushort, short, int, long , double, float, double, bool , char, string
+            // singed means : + and - both 
+            // unsigned means : only 
+            // 1byte = 8 bits
+            //Console.WriteLine(s);
+            //Console.WriteLine(m);
+            //Console.WriteLine(n);
+
+            //========= Operators =============
+
+            //Asingnmet operator : =, +=, *=, -=, /=, %=
+            //Arithmatic operator : +, -, /, %, *
+            //Comaprison opeartor : ==, <=, >=, !=, <, >
+            //Logical opearator : &, &&, |, ||, ^, !
+            //Increment and Decrement : ++, --
+            //Ternary opearator : condition ? true : false
+
+
+            //======= if, if-else, else-if, Nested-if ===========
+
+
+            Console.ReadKey();
+
         }
     }
 }
